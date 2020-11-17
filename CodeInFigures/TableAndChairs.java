78f84bad0cba8dc1a90518fd417ea17834466a28
Shikai754596 <shikai.huang@my.jcu.edu.au>
2020-11-17 18:48:22 +0800

    First commit

---
+++ CodeInFigures/TableAndChairs.java
@@ -0,0 +1,6 @@
+public class TableAndChairs {
+    public static void main(String[] args)
+    {
+        System.out.println("x                      x\nx                      x\nx      xxxxxxxxxx      x\nxxxxx  x        x  xxxxx\nx   x  x        x  x   x\nx   x  x        x  x   x");
+    }
+}
