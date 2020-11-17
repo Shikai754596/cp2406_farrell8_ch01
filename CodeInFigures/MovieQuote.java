6cc5d7515d339f66e8bbec4b8047cafdecedb71c
Shikai754596 <shikai.huang@my.jcu.edu.au>
2020-11-17 18:38:00 +0800

    First commit

--- CodeInFigures/MovieQuote.java
+++ CodeInFigures/MovieQuote.java
@@ -1,6 +1,6 @@
 public class MovieQuote {
     public static void main(String[] args)
     {
-        System.out.println("I'm Your father!!");
+        System.out.println("Tell me, do you bleed?");
     }
 }
---
+++ CodeInFigures/MovieQuoteInfo.java
@@ -0,0 +1,4 @@
+public class MovieQuoteInfo {
+    public static void main(String[] args)
+    {System.out.println("Tell me, do you bleed? \nBatman v Superman: Dawn of Justice \nBatman \n2016");}
+}
